4588040d3fb0516e421b18fd455b25d3bd79e96d
don <bandy@gmail.com>
2021-08-14 18:50:13 -0700

    lab 11

---
+++ src/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class Profile extends React.Component {
+  render() {
+    return(
+      <h1>NAME, PROFILE, EMAIL ADDRESS</h1>
+    )
+  }
+}
+
+export default Profile;
